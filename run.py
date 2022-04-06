5f632d59cacc61553569062e86323d17a00d37be
van-essa <vanessa.andersson3@gmail.com>
2022-04-06 05:26:39 +0000

    Changed to code to implemnet the board

--- run.py
+++ run.py
@@ -1,48 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-import random
+from random import randint
 
-#Asking player's name 
+# Legend
+# "@" for placing ship
+# " " for available space
+# "X" for hit battleship
+# "-" for missed shot
 
-name = input("Enter your name: ")
-print("\n")
-print("Hello", name + "!")
+# creates a list of 5 spaces, 5 times
+HIDDEN_BOARD = [[" "] * 5 for x in range(5)]
+GUESS_BOARD = [[" "] * 5 for x in range(5)]
+USER_BOARD = [[" "] * 5 for x in range(5)]
 
-#Create a 5x5 2 dimensional list
-board = []
+# converts letters to numbers and numbers to letters
+letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
 
-for x in range(5):
-    board.append(["[_]"] * 5)
-
-def print_board(board):
-    for row in board:
-        print(" ".join(row))
-print("Let's play Battleship!")
-print("Take a guess between 0 and 4 for row & column.\n")
-print(" Battleship Field")
-print("===================")
-
-print_board(board)
-
-
-#Defining the rows and columns
-
-def random_row(board):
-  return random.randint(0,len(board)-1)
-def random_col(board):
-  return random.randint(0,len(board[0])-1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-print (ship_row)
-print (ship_col)
-
-for turn in range(4):
-	guess_row = input("Guess Row:")
-	guess_col = input("Guess Column:")
-
-
-
-
-
+numbers_to_letters = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F"}
